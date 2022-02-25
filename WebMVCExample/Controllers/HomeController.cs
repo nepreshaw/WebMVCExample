846a3ec3c4d1b325c22871eb8465ed39328d3873
Nicholas Preshaw <nickpreshaw@yahoo.com>
2022-02-25 11:20:02 -0500

    Add project files.

---
+++ WebMVCExample/Controllers/HomeController.cs
@@ -0,0 +1,25 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using WebMVCExample.Models;
+
+namespace WebMVCExample.Controllers {
+    public class HomeController : Controller {
+
+        public IActionResult Index() {
+            //viewbag is a built in dictionary. created so controllers can pass in data to the views
+            //could createa a model for name and fv instead but would have to createa a seperate view
+            ViewBag.Name = "Mary";
+            ViewBag.FV = 0;
+            return View();
+        }
+
+        [HttpPost]
+        public IActionResult Index(FutureValueModel model) {
+            ViewBag.FV = model.CalculateFutureValue();
+            return View(model);
+        }
+    }
+}
---
+++ WebMVCExample/Models/FutureValueModel.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace WebMVCExample.Models {
+    public class FutureValueModel {
+        public decimal MonthlyInvestment { get; set; }
+        public decimal YearlyInterestRate { get; set; }
+        public int Years { get; set; }
+
+        public decimal CalculateFutureValue() {
+            var months = Years * 12;
+            var monthlyInterestRate = YearlyInterestRate / 12 / 100;
+            var futureValue = 0m;
+            for(var i = 0; i < months; i++) {
+                futureValue = (futureValue + MonthlyInvestment) * (1 + monthlyInterestRate);
+            }
+            return futureValue;
+        }
+    }
+}
